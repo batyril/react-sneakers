19c64050fc2b5391de244fe66de3a9d330a98172
batyril <237x237@gmail.ru>
2023-07-24 21:55:09 +0300

    fix: remove unnecessary npm libraries

--- src/main.tsx
+++ src/main.tsx
@@ -4,19 +4,14 @@ import { App } from './components/App';
 import './scss/index.scss';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { store } from './store';
-
-const client = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <QueryClientProvider client={client}>
-        <BrowserRouter basename='/react-sneakers/'>
-          <App />
-        </BrowserRouter>
-      </QueryClientProvider>
+      <BrowserRouter basename='/react-sneakers/'>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
